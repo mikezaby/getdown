d7617c85a9cce5d3bd48235b86a13d9b81a331bb
mikezaby <mikezaby@gmail.com>
2016-01-24 03:30:25 +0200

    User thread for parallel downloading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate threadpool;
 extern crate hyper;
 extern crate crypto;
 extern crate rustc_serialize;
@@ -8,10 +9,25 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 
+use threadpool::ThreadPool;
+use std::sync::mpsc;
+
 fn main() {
     let file = File::open("input.txt").unwrap();
     let file = BufReader::new(file);
+
+    let thread_pool = ThreadPool::new(100);
+    let (sender, receiver) = mpsc::channel();
+
     for line in file.lines() {
-        downloader::md5(&line.unwrap());
+        let sender = sender.clone();
+
+        thread_pool.execute(move || {
+            sender.send(downloader::sha1(&line.unwrap())).unwrap();
+        });
+    }
+
+    while thread_pool.active_count() > 0 {
+        println!("{}", receiver.recv().unwrap());
     }
 }
